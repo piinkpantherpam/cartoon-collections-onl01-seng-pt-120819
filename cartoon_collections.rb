d3a5d40c6a62cee0efaea20fd9e66c05ff33eb12
Pam Cairo <pcairo_90@outlook.com>
2019-12-21 05:43:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,6 @@ end
 
 roll_call_dwarves(dwarves)
 
-
-
 def summon_captain_planet(array)
   nu_array = []
   i = 0 
@@ -36,9 +34,9 @@ def long_planeteer_calls(array)
   end 
 end
 
+long_planeteer_calls(call_screams)
 
-
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
