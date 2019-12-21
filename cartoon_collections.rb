4252074d7127d3773a3623d35b4ede4eae8765f9
Pam Cairo <pcairo_90@outlook.com>
2019-12-21 05:46:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -42,8 +42,9 @@ snacks = ["crackers", "gouda", "thyme"]
   end 
 
 def find_the_cheese(array)
-  
   cheese_types = ["cheddar", "gouda", "camembert"]
+    array.find do |type|
+    cheese_types.include?(type)
   end
 end
 
